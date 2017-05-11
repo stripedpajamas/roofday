3962ee900daedfe9a1bad4fcbd81c08b0bf7fe66
Nathan Johnson <najohnson@redventures.com>
2017-05-11 15:50:04 -0400

    Changing roof

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var roofday = (Math.floor(Math.random() * 100) >= 50) ? 'YES' : 'NO';
+  res.render('index', { title: roofday });
 });
 
 module.exports = router;
