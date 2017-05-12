3c8806497611d2427da7c95c983cdbb7b85faa1d
Nathan Johnson <najohnson@redventures.com>
2017-05-11 21:21:15 -0400

    Adding APIs

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,98 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const Promise = require('bluebird');
+const request = require('request');
+const moment = require('moment');
+
+const router = express.Router();
+
+const tempWeights = {
+  68: 1,
+  69: 2,
+  70: 3,
+  71: 4,
+  72: 7,
+  73: 10,
+  74: 13,
+  75: 16,
+  76: 15,
+  77: 14,
+  78: 13,
+  79: 12,
+  80: 11,
+  81: 10,
+  82: 9,
+  83: 8,
+  84: 7,
+  85: 6,
+  86: 5,
+  87: 4,
+  88: 3,
+  89: 2,
+  90: 1
+};
+
+const calculateRoof = (temp, precip, wind, uv) => {
+  // Rain change over 35% is a no go
+  if (precip >= 0.35) return false;
+
+  // Get the current weight of the temperature
+  const tmpWeight = tempWeights[Math.floor(temp)] || 0;
+
+  // roofday is temperature weight times wind minus UV
+  const windWeight = wind > 20 ? 0 : wind * -1;
+  if (windWeight === 0) return false;
+
+  // UV Weight is 1.5 * the UV but it's bad so we weight it negatively
+  const uvWeight = uv * -1.5;
+  const roofday = tmpWeight + (windWeight - uvWeight);
+
+  return roofday > 9.5;
+};
+
+const getWeather = (location) => {
+  const url = `http://api.openweathermap.org/data/2.5/weather?${location},us&appid=${process.env.WEATHER_KEY}`;
+  return new Promise((resolve, reject) => {
+    request(url, function (err, response, body) {
+      if (err) console.log(err);
+      resolve(JSON.parse(body));
+    });
+  });
+};
+
+const getUV = (lat, lon) => {
+  const url = `https://iaspub.epa.gov/enviro/efservice/getEnvirofactsUVHOURLY/ZIP/28277/JSON`;
+
+  return new Promise((resolve, reject) => {
+    request(url, function (err, response, body) {
+      if (err) console.log(err);
+      resolve(JSON.parse(body));
+    });
+  });
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var roofday = (Math.floor(Math.random() * 100) >= 50) ? 'YES' : 'NO';
-  res.render('index', { title: roofday });
+  let tmp = 0;
+  let precip = 0;
+  let wind = 0;
+  let clouds = 0;
+
+  getWeather('zip=29707').then((body) => {
+    tmp = (9/5) * (body.main.temp - 273) + 32;
+    precip = body.precipitation ? body.precipitation.value : 0;
+    wind = body.wind.speed;
+    clouds = body.clouds.all;
+
+    return getUV(body.coord.lat, body.coord.lon);
+  }).then((body) => {
+    const now = moment().format('hh A');
+    const time = body.find((datum) => {
+      return datum.DATE_TIME.includes(now);
+    });
+
+    const roofday = calculateRoof(tmp, precip, wind, time.UV_VALUE) ? 'YES' : 'NO';
+    res.render('index', { title: roofday, tmp, precip, wind, uv: time.UV_VALUE, clouds });
+  });
 });
 
 module.exports = router;
